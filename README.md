19acaee1f02d81f3a4eca3216178a1dfd5c5110d
Rachel Conatser <10806252@my.uvu.edu>
2019-09-16 00:51:22 -0600

    Added aesthetics and functionality
    Added different background, changed colorization, added images, updated log statements, and finished JS. See README for list of 'To Dos'

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-monster-slayer
+Monster Slayer program by Rachel Conatser
+With help from Max via Udemy Course
+
+Changes made to Max's Code:
+1. (DONE) Added photos for Player and Monster, added background, changed button colors
+2. (Chat bubbles added, need Functionality) Change Monster Damage to have display next to image in a bubble with sword or heal icon
+3. After 5 log statements, start making log statements fade out
+4. (DONE) Added separate Game Over HTML and CSS files to point to when game is truly over
+5. (DONE) Added Turn Numbers to Log Statements to avoid confusion
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-new Vue ({
+new Vue({
     el: '#app',
     data: {
         playerHealth: 100,
@@ -7,73 +7,158 @@ new Vue ({
         turns: []
     },
     methods: {
-        startGame: function() {
+        startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
-        attack: function() {
-            var damage = this.calculateDamage(3,10);
+        attack: function () {
+            var damage = this.calculateDamage(3, 10);
             this.monsterHealth -= damage;
             this.turns.unshift({
                 isPlayer: true,
-                text: 'Player hits the Monster for ' + damage
+                text: this.turns.length + '. Player hits Monster for ' + damage + " HP."
             });
-            if(this.checkWin()) {
+            if (this.checkWin()) {
                 return;
             }
 
             this.monsterAttacks();
         },
-        specialAttack: function() {
-            this.monsterHealth -= this.calculateDamage(10,20);
-            if(this.checkWin()) {
-                return;
-            }
-
-            this.monsterAttacks();
-        },
-        monsterAttacks: function() {
-            var damage = this.calculateDamage(5,12);
-            this.playerHealth -= damage;
-            this.checkWin();
-
+        specialAttack: function () {
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
             this.turns.unshift({
-                isPlayer: false,
-                text: 'Monster hits the Player for ' + damage
+                isPlayer: true,
+                text: this.turns.length + '. Player hits Monster hard for ' + damage + " HP."
             });
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
         },
-        heal: function() {
-            if(this.playerHealth <= 90) {
+        heal: function () {
+            if (this.playerHealth <= 90) {
                 this.playerHealth += 10;
             } else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: this.turns.length + '. Player heals for 10 HP.'
+            });
             this.monsterAttacks();
         },
-        giveUp: function() {
+        giveUp: function () {
             this.gameIsRunning = false;
+        },
+        monsterAttacks: function() {
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: this.turns.length + '. Monster hits Player for ' + damage + " HP."
+            });
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
-        checkWin: function() {
-            if(monsterHealth <= 0){
-                if(confirm('You won! Would you like to play again?')) {
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! Would you like to play again?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
+                    this.gameOver();
                 }
                 return true;
-            } else if(playerHealth <= 0) {
-                if(confirm('You lost! Would you like to play again?')) {
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! Would you like to play again?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
+                    this.gameOver();
                 }
                 return true;
             }
             return false;
+        },
+        gameOver: function () {
+            window.open("game-over.html");
         }
     }
 });
+
+
+
+
+// Older Code, for Reference if Needed
+
+// // register modal component
+// Vue.component('modal', {
+//     template: '#modal-template'
+//     })
+
+//     data: {
+//         playerHealth: 100,
+//         monsterHealth: 100,
+//         gameIsRunning: false,
+//         turns: [],
+//         showModal: false,
+//         response: "", // blank text, will be filled with responses as game progresses
+//         modalTitle: "",
+//         randomNum: 0;
+//     },
+//     methods: {
+//         startGame: function () {
+//             this.gameIsRunning = true;
+//             this.playerHealth = 100;
+//             this.monsterHealth = 100;
+//             this.turns = [];
+//             this.showModal = false;
+//         },
+//         poison: function () {
+//             var isTrue = 3;
+//             if(isTrue){
+//                 if(this.turns.length)
+//                 return true;
+//             }else{
+//                 return false;
+//             }
+//             return false;
+//         },
+//         giveUp: function () {
+//             this.modalTitle = "Game Over!";
+//             this.response = "Sorry to see you give up...";
+//             this.showModal = true;
+//         },
+//         monsterAttacks: function() {
+//             var damage = this.calculateDamage(5, 12);
+//             this.playerHealth -= damage;
+//             this.randomNum = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+//             poison(randomNum);
+
+//             this.checkWin();
+//             this.turns.unshift({
+//                 isPlayer: false,
+//                 text: this.turns.length + '. Monster hits Player for ' + damage + ' Points.'
+//             });
+//         },    
+//         checkWin: function() {
+//             if (this.monsterHealth <= 0) { // Player Wins!
+//                 this.modalTitle = "Victory!";
+//                 this.response = "Congratulations, You Win!";
+//                 this.showModal = true; // Modal pops up
+//                 return true;
+//             } else if (this.playerHealth <= 0) { // Player Loses!
+//                 this.modalTitle = "Game Over!";
+//                 this.response = "Sorry, You Lost!";
+//                 this.showModal = true; // Modal pops up
+//                 return false;
+//             }
+//         },
+//         gameOver: function() {
+//             window.open("game-over.html");  
+//         }
